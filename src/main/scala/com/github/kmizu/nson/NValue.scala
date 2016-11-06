79bfc1582f8f783f24a6ae2b6150c6c5b9e3139d
Kota Mizushima <mizukota@gmail.com>
2016-11-06 19:27:16 +0900

    NValue, parsed object of NSON, is implemented

---
+++ src/main/scala/com/github/kmizu/nson/NValue.scala
@@ -0,0 +1,29 @@
+import scala.language.dynamics
+
+object NValue {
+  final case class NLong(override val value: Long) extends NValue
+  final case class NDouble(override val value: Double) extends NValue
+  final case class NBoolean(override val value: Boolean) extends NValue
+  final case class NString(override val value: String) extends NValue
+  case object NNull extends NValue {
+    override val value: Null = null
+  }
+  final case class NArray(val content: Seq[NValue]) extends NValue {
+    override val value: Seq[Any] = content.map{_.value}
+
+    override def apply(index: Int): NValue = content(index)
+  }
+  final case class NObject(val content: Map[String, NValue]) extends NValue {
+    override val value: Map[String, Any] = content.map{ case (k, v) => (k, v.value)}
+
+    override def apply(name: String): NValue = content(name)
+
+    override def selectDynamic(name: String): NValue = content(name)
+  }
+}
+sealed trait NValue extends Dynamic {
+  def value: Any
+  def apply(index: Int): NValue = throw new UnsupportedOperationException("NValue#apply(Int)")
+  def apply(name: String): NValue = throw new UnsupportedOperationException("NValue#apply(String)")
+  def selectDynamic(name: String): NValue = throw new UnsupportedOperationException("NValue#selectDynamic(String)")
+}
